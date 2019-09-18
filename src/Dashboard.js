a6a07e910096e8c28c29ddca0264b5b2eb9b9e69
Ashwani Goswami <goswami17829@gmail.com>
2019-09-18 10:33:09 +0530

    filter by search name

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
     state = {
         data: ["itemOne", "itemTwo"],
+        arr: [
+            {name: 'ashwani goswami', option: 'option1'},
+            {name: 'ruchi', option: 'option2'},
+            {name: 'abhishek', option: 'option3'}
+        ],
         itemOne: false,
         itemTwo: false,
+        dropdownVal: '',
+        searchByNameVal: ''
     }
 handleChange = (value) => {
     var check1 = document.querySelector('.itemOne');
@@ -23,6 +30,17 @@ handleChange = (value) => {
         })
     }
 }
+handleSearchByName = (e) => {
+    this.setState({
+        [e.target.name] : e.target.value
+    })
+}
+handleSubmit = (e) => {
+    e.preventDefault();
+    const inputVal = this.state.searchByNameVal;
+    const filteredArr = this.state.arr.filter(item => item.name.includes(inputVal) && item.option === this.state.dropdownVal);
+    console.log(filteredArr)
+} 
     render() {
         const { itemOne, itemTwo } = this.state;
         return (
@@ -36,6 +54,14 @@ handleChange = (value) => {
                 {
                     itemTwo ? <div>itemtwo</div> : null
                 }
+                <select name="dropdownVal" onChange={this.handleSearchByName}>
+                    <option value="option1">option1</option>
+                    <option value="option2">option2</option>
+                    <option value="option3">option3</option>
+                    <option value="option4">option4</option>
+                </select>
+                <input type="text" placeholder="search by name" name="searchByNameVal" onChange={this.handleSearchByName} />
+                <button onClick={this.handleSubmit}>Search</button>
             </div>
         );
     }
